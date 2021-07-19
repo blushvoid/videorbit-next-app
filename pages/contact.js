91f4e7845d8e81b870b63df22282ee22f06fb629
blushvoid <a.e.ignac@gmail.com>
2021-07-19 09:49:45 +0200

    feature: contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ContactForm from "../components/contactForm";
+import ResponsiveHeader from "../components/responsiveHeader";
+import Footer from "../components/footer";
 
 const Contact = () => (
-  <div>
-    <ContactForm />
+  <div className="contactPage">
+    <ResponsiveHeader style={{ zIndex: 9999, color: "#FFF" }} />
+    <div className="formContainer">
+      <ContactForm />
+    </div>
+    <Footer />
   </div>
 );
 
