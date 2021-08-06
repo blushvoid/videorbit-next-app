5767dd5dac39818191feab0edcd3c2a4f3131f88
blushvoid <a.e.ignac@gmail.com>
2021-08-06 12:44:55 +0200

    fix: buttons

--- components/linksContainer.js
+++ components/linksContainer.js
@@ -6,60 +6,60 @@ function LinksContainer() {
       <div className="buttonsContainer">
         <h1>Links</h1>
         <a href="https://videorbit.com/" rel="noreferrer">
-          <button className="buttonAnimated">Website</button>
+          <button className="buttonAnimated linksButton">Website</button>
         </a>
         <a href="https://www.instagram.com/videorbit.studio/" rel="noreferrer">
-          <button className="buttonAnimated">Instagram</button>
+          <button className="buttonAnimated linksButton">Instagram</button>
         </a>
         <a href="https://www.facebook.com/VideOrbitStudio" rel="noreferrer">
-          <button className="buttonAnimated">Facebook</button>
+          <button className="buttonAnimated linksButton">Facebook</button>
         </a>
         <a href="https://www.snapchat.com/add/videorbit" rel="noreferrer">
-          <button className="buttonAnimated">Snapchat</button>
+          <button className="buttonAnimated linksButton">Snapchat</button>
         </a>
         <a href="https://www.linkedin.com/company/videorbit" rel="noreferrer">
-          <button className="buttonAnimated">Linkedin</button>
+          <button className="buttonAnimated linksButton">Linkedin</button>
         </a>
         <a href="https://ello.co/videorbit">
-          <button className="buttonAnimated">ELLO</button>
+          <button className="buttonAnimated linksButton">ELLO</button>
         </a>
         <br />
         <h1>Press</h1>
         <a href="https://sparkar.facebook.com/blog/women-redefining-reality/?utm_medium=sparkar_group&utm_source=facebook&utm_campaign=organic&fbclid=IwAR1JhvywvC1fgM3u73rG-JC6PxR3pMSRAjjXc-Z4Y4dzWzyKk_2QYAllGTA" rel="noreferrer">
-          <button className="buttonAnimated">
+          <button className="buttonAnimated linksButton">
             Women Redefining Reality | Facebook Spark AR
           </button>
         </a>
         <a href="https://www.wethecoolmagazine.com/stories-issue-no-5/the-beautifully-chaotic-ar-world-of-isabelle-udo" rel="noreferrer">
-          <button className="buttonAnimated">
+          <button className="buttonAnimated linksButton">
             The beautifully chaotic AR world of Isabelle Udo | We The Cool
             Magazine
           </button>
         </a>
         <a href="https://sparkar.facebook.com/blog/spark-ar-creator-isabelle-udo/" rel="noreferrer">
-          <button className="buttonAnimated">
+          <button className="buttonAnimated linksButton">
             Spark AR Creator Q&A: Isabelle Udo | Facebook Spark AR
           </button>
         </a>
         <a href="https://www.youtube.com/watch?v=ldwDTw3bLoo" rel="noreferrer">
-          <button className="buttonAnimated">
+          <button className="buttonAnimated linksButton">
             Mark Zuckerberg on the future of AR
           </button>
         </a>
         <a href="https://ello.co/elloblog/post/trux9tpbwu5etzh5rkvncw" rel="noreferrer">
-          <button className="buttonAnimated">
+          <button className="buttonAnimated linksButton">
             In Your Words: Between two AR Artists, Isabelle Udo and Harun
             Köktürk | ELLO
           </button>
         </a>
         <a href="https://observer.com/2020/12/isabelle-udo-interview-digital-art-month-miami/" rel="noreferrer">
-          <button className="buttonAnimated">
+          <button className="buttonAnimated linksButton">
             Isabelle Udo's Augumented Reality Art Takes Over Miami With Digital
             Art Month | OBSERVER
           </button>
         </a>
         <a href="https://blog.lenslist.co/2020/06/23/ar-talks-isabelle-udo/" rel="noreferrer">
-          <button className="buttonAnimated">
+          <button className="buttonAnimated linksButton">
             AR talks | Isabelle Udo | LensList
           </button>
         </a>
